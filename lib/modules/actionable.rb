b5698c8aa0df468085758ea68b3d63b6719d44d1
Sten Hägglund <sten.hagglund@gmail.com>
2012-10-14 22:28:34 +0300

    cleared some scaffolded/old unused stuff out + added a missing migration to git

--- lib/modules/actionable.rb
+++
@@ -1,7 +0,0 @@
-module Actionable
-  def self.included(base)
-    base.send :attr_accessor, :action_by
-    base.send :attr_accessible, :action_by
-    base.send :validates, :action_by, :presence => true
-  end
-end
