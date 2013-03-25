10c2e0fe0071a602b30c2e0d474f6129f3aa9abc
Sten Hägglund <sten.hagglund@gmail.com>
2013-03-25 19:43:25 +0200

    removed hidden app config as this won't be going up publicly

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kassa::Application.config.secret_token = APP_CONFIG['secret_token']
+Kassa::Application.config.secret_token = '3e621469f464098407fc6a7752ec783c143e022c034419fb0208eca952653bd1eae54cad05e3c6452070361aa7f82ab10fa2feeb596889938f1a0cc5dc90afef'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,4 +7,4 @@
 # (create the session table with "rails generate session_migration")
 Kassa::Application.config.session_store :active_record_store,
                                         key: '_kassa_session',
-                                        secret: APP_CONFIG['session_secret']
+                                        secret: '3e621469f464098407fc6a7752ec783c143e022c034419fb0208eca952653bd1eae54c'
