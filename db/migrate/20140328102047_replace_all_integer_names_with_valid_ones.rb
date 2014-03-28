2fcdd30997bd9a2330aee35a0611ee4c27df0905
Sten Hägglund <sten.hagglund@gmail.com>
2014-03-28 12:27:42 +0200

    Migrate all integer only names to valid ones

---
+++ db/migrate/20140328102047_replace_all_integer_names_with_valid_ones.rb
@@ -0,0 +1,17 @@
+class ReplaceAllIntegerNamesWithValidOnes < ActiveRecord::Migration
+  def change
+    test_regexp = /\A\d+\z/
+    Product.all.each do |p|
+      if p.name =~ test_regexp
+        p.name = "#{p.name} ForceChanged"
+        p.save!
+      end
+    end
+    User.all.each do |p|
+      if p.username =~ test_regexp
+        p.username = "#{p.name} ForceChanged"
+        p.save!
+      end
+    end
+  end
+end
